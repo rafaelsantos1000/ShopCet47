05c9cad0af49c273336b346336cc40eb2e80f411
Rafael Santos <rafaasfs@gmail.com>
2020-10-23 21:55:31 +0100

    Create OrderRepository

---
+++ ShopCet47.Web/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopCet47.Web.Data.Entities;
+using ShopCet47.Web.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCet47.Web.Data.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+
+        public OrderRepository(DataContext context, IUserHelper userHelper) : base(context)
+        {
+            _context = context;
+            _userHelper = userHelper;
+        }
+
+        public async Task<IQueryable<Order>> GetOrderAsync(string username)
+        {
+            var user = await _userHelper.GetUserByEmailAsync(username);
+            if(user == null)
+            {
+                return null;
+            }
+
+            if(await _userHelper.IsUserInRoleAsync(user, "Admin"))
+            {
+                //se o user for o admin
+                return _context.Orders
+                .Include(o => o.Items)
+                .ThenInclude(i => i.Product)
+                .OrderByDescending(o => o.OrderDate);
+
+            }
+
+            //ou users normais
+            return _context.Orders
+                .Include(o => o.Items)
+                .ThenInclude(i => i.Product)
+                .Where(o => o.User == user)
+                .OrderByDescending(o => o.OrderDate);
+        }
+    }
+}
